a4916b513f3176df252a3def188dfd77c0844b10
苏梓威 <suziwei@MacBook-Pro-6.local>
2020-04-10 21:32:33 +1200

    Plugin connect to InfluxDB

--- octoprint_CMfgDataAcquisition/__init__.py
+++ octoprint_CMfgDataAcquisition/__init__.py
@@ -12,8 +12,18 @@ from __future__ import absolute_import
 import octoprint.plugin
 from apscheduler.schedulers.background import BackgroundScheduler
 
-from influxdb import InfluxDBClient
+from influxdb_client import InfluxDBClient, Point
 import requests
+
+
+INFLUXDB_CLOUD_HOST = "https://us-central1-1.gcp.cloud2.influxdata.com"
+INFLUXDB_TOKEN = "E_s7vYYXrSNWdqGKBEJWl4kQhOrjw36s2iYViH0xcLlKu-wawUKjKBcUqhw9rE1VB1z5kEX89uSUqMdRgMZWVQ=="
+
+INFLUXDB_ORGID = "c9fb97df591a9e5f"
+INFLUXDB_BUCKETID = "7ca03f47b608c3f3"
+
+X_API_KEY = "F07E6EF39FE74433BD23541996E98FFA"
+
 
 
 # job related data
@@ -22,19 +32,13 @@ def data_convert_job(data):
     job_data = data.get("job")
     progress_data = data.get("progress")
 
-    converted_data.append({
-        "measurement": "job_progress",
-        "tags": {
-            "X-API-KEY": "xxxxxxxx1"
-        },
-        "fields": {
-            "completion": progress_data.get("completion") if progress_data.get("completion") is not None else 0.0,
-            "filepos": progress_data.get("filepos"),
-            "printTime":progress_data.get("printTime"),
-            "printTimeLeft": progress_data.get("printTimeLeft"),
-            "printTimeLeftOrigin": progress_data.get("printTimeLeftOrigin"),
-        }
-    })
+    job_progress_point = Point("job_progress").tag("X-API-KEY", "xxxxxxxx1")\
+        .field("completion", progress_data.get("completion") if progress_data.get("completion") is not None else 0.0)\
+        .field("filepos", progress_data.get("filepos"))\
+        .field("printTime", progress_data.get("printTime"))\
+        .field("printTimeLeft", progress_data.get("printTimeLeft"))\
+        .field("printTimeLeftOrigin", progress_data.get("printTimeLeftOrigin"))
+    converted_data.append(job_progress_point)
 
     if job_data.get("filament") is None:
         return converted_data
@@ -43,27 +47,23 @@ def data_convert_job(data):
         if filament_info is None:
             break
 
-        converted_data.append({
-            "measurement": "job_info",
-            "tags": {
-                "X-API-KEY": "xxxxxxxx1",
-                "filament": filament_toolname
-            },
-            "fields": {
-                "averagePrintTime": job_data.get("averagePrintTime", 0.0) if filament_info.get("averagePrintTime") is None else 0.0,
-                "estimatedPrintTime": job_data.get("averagePrintTime", 0.0) if filament_info.get("estimatedPrintTime") is None else 0.0,
-                "filament_length": filament_info.get("length", 0.0) if filament_info.get("length") is None else 0.0,
-                "filament_volume": filament_info.get("volume", 0.0) if filament_info.get("volume") is None else 0.0,
-                "file_date": job_data.get("file").get("date"),
-                "file_display": job_data.get("file").get("display"),
-                "file_name": job_data.get("file").get("name"),
-                "file_origin":job_data.get("file").get("origin"),
-                "file_path": job_data.get("file").get("path"),
-                "file_size": job_data.get("file").get("size"),
-                "lastPrintTime": job_data.get("lastPrintTime", 0.0) if filament_info.get("lastPrintTime") is None else 0.0,
-                "user": job_data.get("user")
-            }
-        })
+        job_info_point = Point("job_info").tag("X-API-KEY", "xxxxxxxx1")\
+            .tag("filament", filament_toolname) \
+            .field("averagePrintTime",
+                   job_data.get("averagePrintTime", 0.0) if filament_info.get("averagePrintTime") is None else 0.0) \
+            .field("estimatedPrintTime", job_data.get("averagePrintTime", 0.0) if filament_info.get("estimatedPrintTime") is None else 0.0) \
+            .field("filament_length", filament_info.get("length", 0.0) if filament_info.get("length") is None else 0.0) \
+            .field("filament_volume", filament_info.get("volume", 0.0) if filament_info.get("volume") is None else 0.0) \
+            .field("file_date", job_data.get("file").get("date"))\
+            .field("file_display", job_data.get("file").get("display")) \
+            .field("file_name", job_data.get("file").get("name")) \
+            .field("file_origin", job_data.get("file").get("origin"))\
+            .field("file_path", job_data.get("file").get("path"))\
+            .field("file_size", job_data.get("file").get("size")) \
+            .field("lastPrintTime", job_data.get("lastPrintTime", 0.0) if filament_info.get("lastPrintTime") is None else 0.0) \
+            .field("user", job_data.get("user"))
+
+        converted_data.append(job_info_point)
 
     return converted_data
 
@@ -72,105 +72,106 @@ def load_data_job():
     url = "http://octopi.local/api/job"
 
     headers = {
-        "X-Api-Key": "F07E6EF39FE74433BD23541996E98FFA",
+        "X-Api-Key": X_API_KEY,
         "Content-Type": "application/json"
     }
     res_job = requests.get(url, headers=headers).json()
+    print(res_job)
 
-    dbClient = InfluxDBClient(host='localhost', port=8086, username='root', password='root', database="myPluginDB")
-    dbClient.write_points(data_convert_job(res_job))
+    dbClient = InfluxDBClient(url=INFLUXDB_CLOUD_HOST, token=INFLUXDB_TOKEN)
+    write_api = dbClient.write_api()
+
+    for data_point in data_convert_job(res_job):
+        write_api.write(INFLUXDB_ORGID, INFLUXDB_ORGID, data_point)
 
 
 # connection related data
+
 def data_convert_connection(data):
     converted_data = []
     current_data = data.get("current")
 
-    converted_data.append({
-        "measurement": "connection_current",
-        "tags": {
-            "X-API-KEY": "xxxxxxxx1"
-        },
-        "fields": {
-            "baudrate": current_data.get("baudrate"),
-            "port": current_data.get("port"),
-            "printerProfile": current_data.get("printerProfile"),
-            "state": current_data.get("state"),
-        }
-    })
+    connection_current_point = Point("connection_current").tag("X-API-KEY", "xxxxxxxx1")\
+        .field("baudrate", current_data.get("baudrate"))\
+        .field("port", current_data.get("port"))\
+        .field("printerProfile", current_data.get("printerProfile"))\
+        .field("state", current_data.get("state"))
+    converted_data.append(connection_current_point)
 
     return converted_data
-
 
 def load_data_connection():
     url = "http://octopi.local/api/connection"
 
     headers = {
-        "X-Api-Key": "F07E6EF39FE74433BD23541996E98FFA",
+        "X-Api-Key": X_API_KEY,
         "Content-Type": "application/json"
     }
     res_connection = requests.get(url, headers=headers).json()
+    print(res_connection["current"])
 
-    dbClient = InfluxDBClient(host='localhost', port=8086, username='root', password='root', database="myPluginDB")
-    dbClient.write_points(data_convert_connection(res_connection))
+    dbClient = InfluxDBClient(url=INFLUXDB_CLOUD_HOST, token=INFLUXDB_TOKEN)
+    write_api = dbClient.write_api()
+
+    for data_point in data_convert_connection(res_connection):
+        write_api.write(INFLUXDB_ORGID, INFLUXDB_ORGID, data_point)
+
 
 
 # printer related data
+
 def data_convert_printer(data):
     converted_data = []
     state_data = data.get("state")
     temperature_data = data.get("temperature")
 
-    converted_data.append({
-        "measurement": "printer_state",
-        "tags": {
-            "X-API-KEY": "xxxxxxxx1"
-        },
-        "fields": {
-            "flags_cancelling": state_data.get("flags").get("cancelling"),
-            "flags_closedOrError": state_data.get("flags").get("closedOrError"),
-            "flags_error": state_data.get("flags").get("error"),
-            "flags_finishing": state_data.get("flags").get("finishing"),
-            "flags_operational": state_data.get("flags").get("operational"),
-            "flags_paused": state_data.get("flags").get("paused"),
-            "flags_pausing": state_data.get("flags").get("pausing"),
-            "flags_printing": state_data.get("flags").get("printing"),
-            "flags_ready": state_data.get("flags").get("ready"),
-            "flags_resuming": state_data.get("flags").get("resuming"),
-            "flags_sdReady": state_data.get("flags").get("sdReady"),
-            "text": state_data.get("text")
-        }
-    })
+    printer_state_point = Point("printer_state").tag("X-API-KEY", "xxxxxxxx1")\
+        .field("flags_cancelling", state_data.get("flags").get("cancelling"))\
+        .field("flags_closedOrError", state_data.get("flags").get("error"))\
+        .field("flags_finishing", state_data.get("flags").get("finishing"))\
+        .field("flags_operational", state_data.get("flags").get("operational"))\
+        .field("flags_paused", state_data.get("flags").get("paused"))\
+        .field("flags_pausing", state_data.get("flags").get("pausing"))\
+        .field("flags_printing", state_data.get("flags").get("printing"))\
+        .field("flags_ready", state_data.get("flags").get("ready"))\
+        .field("flags_resuming", state_data.get("flags").get("resuming"))\
+        .field("flags_sdReady", state_data.get("flags").get("sdReady"))\
+        .field("text", state_data.get("text"))
+    converted_data.append(printer_state_point)
 
-    converted_data.append({
-        "measurement": "printer_temperature",
-        "tags": {
-            "X-API-KEY": "xxxxxxxx1"
-        },
-        "fields": {
-            "bed_actual": temperature_data.get("bed").get("actual") if temperature_data.get("bed").get("actual") is not None else 0.0,
-            "bed_offset": temperature_data.get("bed").get("offset") if temperature_data.get("bed").get("offset") is not None else 0.0,
-            "bed_target": temperature_data.get("bed").get("target") if temperature_data.get("bed").get("target") is not None else 0.0,
-            "tool0_actual": temperature_data.get("tool0").get("actual") if temperature_data.get("tool0").get("actual") is not None else 0.0,
-            "tool0_offset": temperature_data.get("tool0").get("offset") if temperature_data.get("tool0").get("offset") is not None else 0.0,
-            "tool0_target": temperature_data.get("tool0").get("target") if temperature_data.get("tool0").get("target") is not None else 0.0
-        }
-    })
+    printer_temperature_point = Point("printer_temperature").tag("X-API-KEY", "xxxxxxxx1")\
+        .field("bed_actual", temperature_data.get("bed").get("actual") if temperature_data.get("bed").get("actual") is not None else 0.0)\
+        .field("bed_offset", temperature_data.get("bed").get("offset") if temperature_data.get("bed").get("offset") is not None else 0.0)\
+        .field("bed_target", temperature_data.get("bed").get("target") if temperature_data.get("bed").get("target") is not None else 0.0)\
+        .field("tool0_actual", temperature_data.get("tool0").get("actual") if temperature_data.get("tool0").get("actual") is not None else 0.0)\
+        .field("tool0_offset", temperature_data.get("tool0").get("offset") if temperature_data.get("tool0").get("offset") is not None else 0.0)\
+        .field("tool0_target", temperature_data.get("tool0").get("target") if temperature_data.get("tool0").get("target") is not None else 0.0)
+    converted_data.append(printer_temperature_point)
 
     return converted_data
+
 
 def load_data_printer():
     url = "http://octopi.local/api/printer"
 
     headers = {
-        "X-Api-Key": "F07E6EF39FE74433BD23541996E98FFA",
+        "X-Api-Key": X_API_KEY,
         "Content-Type": "application/json"
     }
     res_printer = requests.get(url, headers=headers).json()
+    print(res_printer["state"]["flags"])
+    print(res_printer["temperature"]["bed"])
+    print(res_printer["temperature"]["tool0"])
 
-    dbClient = InfluxDBClient(host='localhost', port=8086, username='root', password='root', database="myPluginDB")
-    dbClient.write_points(data_convert_printer(res_printer))
+    dbClient = InfluxDBClient(url=INFLUXDB_CLOUD_HOST, token=INFLUXDB_TOKEN)
+    write_api = dbClient.write_api()
 
+    for data_point in data_convert_printer(res_printer):
+        write_api.write(INFLUXDB_ORGID, INFLUXDB_ORGID, data_point)
+
+
+
+# printer profiles related data
 
 def data_convert_printer_profiles(data, current_profile):
     converted_data = []
@@ -184,41 +185,29 @@ def data_convert_printer_profiles(data, current_profile):
     specification_extruder = current_profile.get("extruder")
     specification_volume = current_profile.get("volume")
 
-    converted_data.append({
-        "measurement": "printer_profiles_specification",
-        "tags": {
-            "X-API-KEY": "xxxxxxxx1",
-            "current_profile": current_profile
-        },
-        "fields": {
-            "specification_axes_e_inverted": specification_axes_e.get("inverted"),
-            "specification_axes_e_speed": specification_axes_e.get("speed"),
-
-            "specification_axes_x_inverted": specification_axes_x.get("inverted"),
-            "specification_axes_x_speed": specification_axes_x.get("speed"),
-
-            "specification_axes_y_inverted": specification_axes_y.get("inverted"),
-            "specification_axes_y_speed": specification_axes_y.get("speed"),
-
-            "specification_axes_z_inverted": specification_axes_z.get("inverted"),
-            "specification_axes_z_speed": specification_axes_z.get("speed"),
-
-            "specification_extruder_count": specification_extruder.get("count"),
-            "specification_extruder_nozzleDiameter": specification_extruder.get("nozzleDiameter") if specification_extruder.get("nozzleDiameter") is not None else 0.0,
-            "specification_extruder_sharedNozzle": specification_extruder.get("sharedNozzle"),
-
-            "specification_volume_custom_box": specification_volume.get("custom_box"),
-            "specification_volume_depth": specification_volume.get("depth") if specification_extruder.get("depth") is not None else 0.0,
-            "specification_volume_formFactor": specification_volume.get("formFactor"),
-            "specification_volume_height": specification_volume.get("height") if specification_extruder.get("height") is not None else 0.0,
-            "specification_volume_origin": specification_volume.get("origin"),
-            "specification_volume_width": specification_volume.get("width") if specification_extruder.get("width") is not None else 0.0,
-
-            "id": current_profile.get("id"),
-            "model": current_profile.get("model"),
-            "name": current_profile.get("name")
-        }
-    })
+    printer_profiles_specification_point = Point("printer_profiles_specification").tag("X-API-KEY", "xxxxxxxx1")\
+            .tag("current_profile", current_profile)\
+        .field("specification_axes_e_inverted", specification_axes_e.get("inverted"))\
+        .field("specification_axes_e_speed", specification_axes_e.get("speed"))\
+        .field("specification_axes_x_inverted", specification_axes_x.get("inverted"))\
+        .field("specification_axes_x_speed", specification_axes_x.get("speed"))\
+        .field("specification_axes_y_inverted", specification_axes_y.get("inverted"))\
+        .field("specification_axes_y_speed", specification_axes_y.get("speed"))\
+        .field("specification_axes_z_inverted", specification_axes_z.get("inverted"))\
+        .field("specification_axes_z_speed", specification_axes_z.get("speed"))\
+        .field("specification_extruder_count", specification_extruder.get("count"))\
+        .field("specification_extruder_nozzleDiameter", specification_extruder.get("nozzleDiameter") if specification_extruder.get("nozzleDiameter") is not None else 0.0)\
+        .field("specification_extruder_sharedNozzle", specification_extruder.get("sharedNozzle"))\
+        .field("specification_volume_custom_box", specification_volume.get("custom_box"))\
+        .field("specification_volume_depth", specification_volume.get("depth") if specification_extruder.get("depth") is not None else 0.0)\
+        .field("specification_volume_formFactor", specification_volume.get("formFactor"))\
+        .field("specification_volume_height", specification_volume.get("height") if specification_extruder.get("height") is not None else 0.0)\
+        .field("specification_volume_origin", specification_volume.get("origin"))\
+        .field("specification_volume_width", specification_volume.get("width") if specification_extruder.get("width") is not None else 0.0)\
+        .field("id", current_profile.get("id"))\
+        .field("model", current_profile.get("model"))\
+        .field("name", current_profile.get("name"))
+    converted_data.append(printer_profiles_specification_point)
 
     return converted_data
 
@@ -227,17 +216,28 @@ def load_data_printer_profile():
     url_connection = "http://octopi.local/api/connection"
 
     headers = {
-        "X-Api-Key": "F07E6EF39FE74433BD23541996E98FFA",
+        "X-Api-Key": X_API_KEY,
         "Content-Type": "application/json"
     }
     res_connection = requests.get(url_connection, headers=headers).json()
+    print(res_connection["current"])
     current_profile = res_connection["current"].get("printerProfile", "_default")
+
     url_printerprofiles = "http://octopi.local/api/printerprofiles"
 
     res_printer_profiles = requests.get(url_printerprofiles, headers=headers).json()["profiles"]
+    print(res_printer_profiles[current_profile]["axes"])
+    print(res_printer_profiles[current_profile]["extruder"])
+    print(res_printer_profiles[current_profile]["id"])
+    print(res_printer_profiles[current_profile]["model"])
+    print(res_printer_profiles[current_profile]["name"])
+    print(res_printer_profiles[current_profile]["volume"])
 
-    dbClient = InfluxDBClient(host='localhost', port=8086, username='root', password='root', database="myPluginDB")
-    dbClient.write_points(data_convert_printer_profiles(res_printer_profiles, current_profile))
+    dbClient = InfluxDBClient(url=INFLUXDB_CLOUD_HOST, token=INFLUXDB_TOKEN)
+    write_api = dbClient.write_api()
+
+    for data_point in data_convert_printer_profiles(res_printer_profiles, current_profile):
+        write_api.write(INFLUXDB_ORGID, INFLUXDB_ORGID, data_point)
 
 
 class CmfgdataacquisitionPlugin(octoprint.plugin.SettingsPlugin,
